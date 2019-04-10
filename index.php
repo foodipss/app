e7a56e142834cd0264f1cb8951169759ef32bbbd
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-04-10 16:13:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -53,10 +53,6 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 <!-- !PAGE CONTENT! -->
 <div class="w3-main" style="margin-left:300px;margin-top:43px;">
 
- 
-
-
-
 
 
   <!-- Footer -->
