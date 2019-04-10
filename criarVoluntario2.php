6c85ae467a918b2e2db100f78db450658b2c5640
ritaabsilva <49485885+ritaabsilva@users.noreply.github.com>
2019-04-10 15:39:04 +0100

    Update criarVoluntario2.php

--- criarVoluntario2.php
+++ criarVoluntario2.php
@@ -1,20 +1,26 @@
+<html>
+<body>
+
 <?php
-session_start();
+require('conexao.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && ($_POST["query"])=='novoVoluntario') {
-    $db = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");
-    $idVoluntario = mysqli_real_escape_string($db,$_POST["idVoluntario"]);
-    $nome = mysqli_real_escape_string($db,$_POST["nome"]);
-    $telefone = mysqli_real_escape_string($db,$_POST["telefone"]);
-    
-    $query = "INSERT INTO voluntario (idVoluntario, nome, telefone) VALUES (4, '$nome', '$telefone')";
-    $queryq = mysqli_query($db, $query);
-    
-}
+$con = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");
+  
+$idVoluntario=$_POST['idVoluntario'];
+$nome=$_POST['nome'];
+$telefone=$_POST['telefone'];
 
-else {
+ 
+ $sql = "INSERT INTO voluntario (idVoluntario, nome, telefone) VALUES (7,'$nome','$telefone')"; 
+if (!$sql){
+  die("Error: 'mysqli_error()'");
+  } else {
    echo "<script> alert('Parabéns ! O seu registo foi aceite.');top.location.href='voluntario.php';</script>";
   }
 
-mysqli_close($db);
+ 
+mysqli_close($con); 
 ?>
+
+</body>
+</html>
