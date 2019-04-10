6e7dc4b71701861dbd5238c2d72ffab6ea616e5f
ritaabsilva <49485885+ritaabsilva@users.noreply.github.com>
2019-04-10 15:50:45 +0100

    Update voluntario.php

--- voluntario.php
+++ voluntario.php
@@ -63,7 +63,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 
 $db = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");
 $query="SELECT idVoluntario, nome, telefone FROM voluntario";
-$resultado= mysqli_query($db, $query);
+$resultado=$mysqli->query($query);
 
 ?>
 
