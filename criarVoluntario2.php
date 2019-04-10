89f10e0aa7e50f824b3318a153c33f7efbd1064f
ritaabsilva <49485885+ritaabsilva@users.noreply.github.com>
2019-04-10 15:54:12 +0100

    Update criarVoluntario2.php

--- criarVoluntario2.php
+++ criarVoluntario2.php
@@ -2,24 +2,26 @@
 <body>
 
 <?php
-require('conexao.php');
 
-$con = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");
+$con = mysql_connect("us-cdbr-iron-east-03.cleardb.net","b74a37105022bd","c0139137");
+if (! $con){
+  die ( "Não foi possível conectar: ​​'mysql_error ()'");
+  } else { 
+  mysql_select_db('heroku_af588fa1a66d1f3', $con);
+  $nome = $_POST['nome']; 
+  $telefone = $_POST['telefone'];
+ 
+  $sql = mysql_query("INSERT INTO voluntario (idVoluntario, nome, telefone) VALUES 
+  (6,'$nome','$telefone')"); 
   
-$idVoluntario=$_POST['idVoluntario'];
-$nome=$_POST['nome'];
-$telefone=$_POST['telefone'];
-
- 
- $sql = "INSERT INTO voluntario (idVoluntario, nome, telefone) VALUES (7,'$nome','$telefone')"; 
-if (!$sql){
-  die("Error: 'mysqli_error()'");
+  if (!$sql){
+	die("Error: 'mysql_error()'");
   } else {
-   echo "<script> alert('Parabéns ! O seu registo foi aceite.');top.location.href='voluntario.php';</script>";
+   echo "<script> alert('Parabéns ! O seu registo foi aceite.');top.location.href='menuadministrador.php';</script>";
   }
-
+}
  
-mysqli_close($con); 
+mysql_close( $con); 
 ?>
 
 </body>
