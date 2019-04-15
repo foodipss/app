74e287f048a5830c9304103382211b531fe20357
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-04-15 13:47:52 +0100

    Update indexAlternativa.php

--- indexAlternativa.php
+++ indexAlternativa.php
@@ -1,1 +1,20 @@
-
+<html lang="pt-pt">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/estilo.css">
+  </head>
+  <body>
+    <nav>
+      <ul>
+        <li><a href="">Fontes</a></li>
+        <li><a href="">Tupperwares</a></li>
+        <li><a href="">Refeições</a></li>
+        <li><a href="">Beneficiários</a></li>
+        <li><a href="">Voluntários</a></li>
+        <li><a href="">Turnos</a></li>
+        <li><a href="">Análises</a></li>
+        <li><a href="">Ajuda</a></li>
+      </ul>
+    </nav>
+  </body>
+</html>
