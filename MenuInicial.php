bd9ebaecfb22e31ae4de8d989e9b3f3c684da2b7
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-04-24 15:14:03 +0100

    Add files via upload

---
+++ MenuInicial.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/estiloMainMenu.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+</head>
+
+<body>
+	<div class="menu col-xs-2">
+		<ul>
+			<li><a href="mostrarFontes.php"><span class="fa fa-home"></span>    Fontes</a></li>
+			<li><a href="pesquisar.php"><span class="fa fa-bars"></span>    Tupperwares</a></li>
+			<li><a href="refeicoes.php"><span class="fa fa-cutlery"></span>    Refeições</a></li>
+			<li><a href="benefeciario.php"><span class="fa fa-address-card"></span>    Beneficiários</a></li>
+			<li><a href="lista_voluntario.php"><span class="fa fa-users"></span>    Voluntários</a></li>
+			<li><a href="indexCalendario.php"><span class="fa fa-calendar"></span>    Turnos</a></li>
+			<li><a href="analise.php"><span class="fa fa-bar-chart"></span>    Análises</a></li>
+			<li><a href="ajuda.php"><span class="fa fa-cog"></span>    Ajuda</a></li>
+			<li><a href="logout.php"><span class="fa fa-circle-o-notch"></span>    Logout</a></li>
+			
+			
+		</ul>
+	</div>
+	
+	<div class="MainContent col-xs-10" id="Content" />
+</body>
+</html>
