32163a7b7dabda1af5f57da66b357704fffed7f7
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-02 14:36:35 +0100

    Add files via upload

---
+++ indexVoluntario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+<link rel="stylesheet" href="css/estiloMainMenu.css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"/>
+</head>
+
+<body>
+	<div class="menu col-xs-2">
+		<ul>
+			<li><a href="analise.php"><span class="fa fa-bar-chart"></span>    Análises</a></li>
+			<li><a href="bens.php"><span class="fa fa-coffee"></span>   	Entrega de bens</a></li>
+			<li><a href="refeicoes.php"><span class="fa fa-cutlery"></span>    Preparação de refeições</a></li>
+			<li><a href="pesquisar.php"><span class="fa fa-bars"></span>    Entrega de refeições</a></li>
+			<li><a href="mostrarFontesVoluntario.php"><span class="fa fa-home"></span>    Fontes</a></li>
+			<li><a href="lista_beneficiario_simples.php"><span class="fa fa-address-card"></span>    Beneficiários</a></li>
+			<li><a href="ajuda.php"><span class="fa fa-cog"></span>    Ajuda</a></li>
+			<li><a href="logout.php"><span class="fa fa-circle-o-notch"></span>    Logout</a></li>
+			
+			
+		</ul>
+	</div>
+	
+	<div class="MainContent col-xs-10" id="Content" />
+</body>
+</html>
