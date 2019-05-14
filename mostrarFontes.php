fc7c6518f5a565e743f06ff8cfda51047703a3c4
ritaabsilva <49485885+ritaabsilva@users.noreply.github.com>
2019-05-14 17:32:11 +0100

    Add files via upload

--- mostrarFontes.php
+++ mostrarFontes.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <title>REFOOD</title>
@@ -5,18 +6,41 @@
 <?php require_once "index.php"; ?>
 
 
+<header class="w3-container" style="padding-top:40px">
+   <center> <h5><b> Lista de Fontes </b></h5></center>
+    <p>
+  </header>
+
 <?php
 $conn = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");;
   
   if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
   } 
-$sql = "SELECT idfonte, nomefonte, moradafonte, emailfonte, contactofonte FROM fonte WHERE visivel='1' ORDER BY nomefonte";
-$result = $conn->query($sql);
-  ?>
+
+// pegar a pagina atual
+$pagina = (isset($_GET['pagina'])) ? $_GET['pagina'] : 1;
+
+$itens_por_pagina = 5;
+
+
+$inicio = ($itens_por_pagina*$pagina)-$itens_por_pagina;
+
+
+$sql = "SELECT idfonte, nomefonte, moradafonte, emailfonte, contactofonte FROM fonte WHERE visivel='1' ORDER BY nomefonte  LIMIT $inicio, $itens_por_pagina";
+  
+$result = $conn->query($sql) or die($conn->error);;
+  
+$num_total = mysqli_num_rows($result);
+
+//defifnir o numero de paginas
+$num_paginas = ceil($num_total/$itens_por_pagina);
+?>
+  
 
 <body>
   <div class="colFonte">
+      <a href="registarFontes.php">Registar nova fonte</a>
   
   <p></p>
   <table class="table">
@@ -56,17 +80,56 @@ $result = $conn->query($sql);
 
 </tbody>
 
-
-
 </table>
+
+<?php
+
+$pagina_anterior = $pagina - 1;
+$pagina_seguinte = $pagina + 1;
+
+?>
+
+<nav class="text-center">
+  <ul class="pager">
+    <li class="previous">  
+
+    <?php 
+      if ($pagina_anterior != 0){ ?>
+
+      <a href="mostrarFontes.php?pagina=<?php echo $pagina_anterior; ?>" >
+       <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+
+    <?php }else { ?>
+      <span class="glyphicon glyphicon-chevron-left"></span>
+
+    <?php } ?>
+
+    </li>
+    <?php 
+      //Apresentar a paginação
+      for($i = 1; $i < $num_paginas; $i++) { ?>
+      <li><a href="mostrarFontes.php?pagina=<?php echo $i; ?>"><?php echo $i ; ?></a></li>
+      <?php } ?>
+
+      <li class="next">  
+
+    <?php 
+      if ($pagina_seguinte <= $num_total){ ?>
+
+      <a href="mostrarFontes.php?pagina=<?php echo $pagina_seguinte ; ?>">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+
+    <?php }else { ?>
+      <span class="glyphicon glyphicon-chevron-right"></span>
+
+    <?php } ?>
+
+    </li>
+
+  </ul>
+</nav>
 </div>
-
-
-<form method="get" action="registarFontes.php" >
-    <button type="submit">Registar nova fonte</button>
-</form>
-
- 
-
 </body>
 </html>
