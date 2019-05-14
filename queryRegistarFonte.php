f5a548dc4907b55b727226c5d3eb4e739a9988f8
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-14 16:07:26 +0100

    Update queryRegistarFonte.php

--- queryRegistarFonte.php
+++ queryRegistarFonte.php
@@ -1,16 +1,21 @@
 <?php 
 
 $db = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");
-$query = "SELECT codigo_fonte FROM fonte WHERE codigo_fonte = $codigo%";
-if(mysql_num_rows($query) > 0)
-{
-echo 'já existe um codigo com este nome';
-}
-else
-{
-	
+
 $nomefonte = $_POST['nomefonte'];
-$codigo_fonte = mysqli_real_escape_string($_POST['codigo_fonte']);
+if(isset($_POST['codigo_fonte'])){
+	 $codigo_fonte = $_POST['codigo_fonte'];
+	
+	$db = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");
+    	$sql = mysqli_query($db, "SELECT * FROM fonte WHERE codigo_fonte = '{$codigo_fonte}'") or print mysql_error();
+
+    #Se o retorno for maior do que zero, diz que já existe um.
+    if(mysqli_num_rows($sql)>0) 
+        echo json_encode(array('codigo_fonte' => 'Ja existe um usuario cadastrado com este email')); 
+    else 
+        echo json_encode(array('codigo_fonte' => 'Usuário valido.' )); 
+}
+};
 $moradafonte = $_POST['moradafonte'];
 $emailfonte = $_POST['emailfonte'];
 $contactofonte = $_POST['contactofonte'];
@@ -24,8 +29,6 @@ else
 {
 	echo 'Inserted';
 }
-}
-
 
 header("refresh:2; url=mostrarFontes.php");
 
