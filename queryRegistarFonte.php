8334b0c3282df4b0ce59232d56ddb0993b0d6332
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-14 15:57:18 +0100

    Update queryRegistarFonte.php

--- queryRegistarFonte.php
+++ queryRegistarFonte.php
@@ -1,9 +1,17 @@
 <?php 
 
 $db = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");
-
+$query = "SELECT codigo_fonte FROM fonte WHERE codigo_fonte = $codigo%";
+if(mysql_num_rows($query) > 0)
+{
+echo 'já existe um codigo com este nome';
+}
+else
+{
+	$codigo_fonte = mysqli_real_escape_string($_POST['codigo_fonte']);
+}
 $nomefonte = $_POST['nomefonte'];
-$codigo_fonte = mysqli_real_escape_string($_POST['codigo_fonte']);
+
 $moradafonte = $_POST['moradafonte'];
 $emailfonte = $_POST['emailfonte'];
 $contactofonte = $_POST['contactofonte'];
