80a64f20ed324834e2da3a79c3fbddfb6655d054
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-14 16:10:08 +0100

    Update queryRegistarFonte.php

--- queryRegistarFonte.php
+++ queryRegistarFonte.php
@@ -3,19 +3,7 @@
 $db = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");
 
 $nomefonte = $_POST['nomefonte'];
-if(isset($_POST['codigo_fonte'])){
-	 $codigo_fonte = $_POST['codigo_fonte'];
-	
-	$db = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");
-    	$sql = mysqli_query($db, "SELECT * FROM fonte WHERE codigo_fonte = '{$codigo_fonte}'") or print mysql_error();
-
-    #Se o retorno for maior do que zero, diz que já existe um.
-    if(mysqli_num_rows($sql)>0) 
-        echo json_encode(array('codigo_fonte' => 'Ja existe um usuario cadastrado com este email')); 
-    else 
-        echo json_encode(array('codigo_fonte' => 'Usuário valido.' )); 
-}
-};
+$codigo_fonte = $_POST['codigo_fonte'];
 $moradafonte = $_POST['moradafonte'];
 $emailfonte = $_POST['emailfonte'];
 $contactofonte = $_POST['contactofonte'];
