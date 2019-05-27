3c663bf663eef20bb17e775da8cae877f2aea9ea
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-27 18:55:13 +0100

    Add files via upload

---
+++ ajudaV.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+<title>REFOOD</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<head>
+  
+   <script>
+   
+  </script>
+
+ </head>
+
+<body> 
+
+<?php require_once "index.php"; ?>
+
+    <h3 align="center" style="font-size:30px">Manual de Utilizador</h3>
+<h4  style="font-size:15px">O presente documento consolida a informacao necessaria a utilizacao da plataforma web REFOOD Guimaraes</h4>
+<br>
+
+<div  style="width:500px;height:400px; float:left;">
+<embed height="100%" width="100%" name="embed_content" src="manualUtilizador.pdf" />
+
+</div>
+
+</body>
+</html>
