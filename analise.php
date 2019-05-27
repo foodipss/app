aac0ec0f5735ae17536e53b183497f98aa873ade
miguelangelo960 <49406812+miguelangelo960@users.noreply.github.com>
2019-05-27 18:49:28 +0100

    Add files via upload

--- analise.php
+++ analise.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
 include_once("conexaoPesquisa.php");
-?>
-
+ $connect2 = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");  
+ $query2 = "SELECT idFonte, count(*) as number FROM bem GROUP BY idFonte";  
+ $result2 = mysqli_query($connect2, $query2); 
+ ?>
 
 <html>
 <head>
@@ -12,11 +14,39 @@ include_once("conexaoPesquisa.php");
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <body>
+
     <?php require_once "index.php"; ?>
     <br>
 
-    <?php 
+             
+           <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
+           <script type="text/javascript">  
+           google.charts.load('current', {'packages':['corechart']});  
+           google.charts.setOnLoadCallback(drawChart);  
+           function drawChart()  
+           {  
+                var data = google.visualization.arrayToDataTable([  
+                          ['idFonte', 'number'],  
+                          <?php  
+                          while($row = mysqli_fetch_array($result2))  
+                          {  
+                               echo "['".$row["idFonte"]."', ".$row["number"]."],";  
+                          }  
+                          ?>  
+                     ]);  
+                var options = {  
+                      title: 'Fontes que doaram o maior numero de bens:',  
+                      is3D:true,  
+                      pieHole: 0.4  
+                     };  
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));  
+                chart.draw(data, options);  
+           }  
+           </script> 
+
+           <br />  
+
+              <?php 
      
       $lista = array();
       $devia = array();
@@ -78,35 +108,16 @@ include_once("conexaoPesquisa.php");
 
     
 
-  <div id="columnchart_values" style="width: 50%; float: right; padding: 5px;"></div>
+  <div id="columnchart_values" style="width: 50%; height: 80%; float: right; padding: 5px;"></div> 
+      </head>  
+     <body>
+           <br /><br />  
+           <div style="width:900px;">  
+              
+                <br />  
+                <div id="piechart" style="width: 70%; height: 80%;"></div>  
+           </div>  
 
-     <script type="text/javascript">
-      google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['F3',     50],
-          ['F2',      20],
-          ['F8',  20],
-          ['F4', 10],
-          ['F1',    7]
-          //['<?php $codigo_fonte ?>', <?php $contador?>],        
-
-      ]);
-        var options = {
-          title: 'Fontes que doaram o maior número de bens',
-           width: 600,
-           height: 400,
-          pieHole: 0.4,
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options);
-      }
-    </script>
-</head>
-  <body>
-  <div id="donutchart" style="width: 50%; height: 50%; padding: 5px; "></div>
-  </body>
-</html>
+           <br />  
+      </body>  
+ </html>  
