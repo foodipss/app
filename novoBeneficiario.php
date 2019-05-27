a29de8c79f9cf92e792ac44e0a5401ffb92e1553
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-27 17:06:23 +0100

    Update novoBeneficiario.php

--- novoBeneficiario.php
+++ novoBeneficiario.php
@@ -56,7 +56,23 @@ input[type=submit]:hover {
   background-color: #FF8000;
 }
 
+button[type=button]:hover {
+  background-color: #FF8000;
+}
+	      input[type=submit] {
+  width: 20%;
+  background-color: #FFBF00;
+  color: white;
+  padding: 14px 20px;
+  
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
 
+button[type=button]:hover {
+  background-color: #FF8000;
+}
       
 
 </style>
@@ -157,7 +173,7 @@ input[type=submit]:hover {
                 <p></p>
                 <input type='hidden' name='query' value="NovoBeneficiário">
                 <input type="submit" value="Registar Beneficiário" id="validar" class="btn btn-primary">
-                <input type="submit" value="Cancelar" onClick="history.go(-1)" class="btn btn-primary"></input>
+                 <button type="button" value="Cancelar" onClick="history.go(-1)" class="btn btn-primary"> Cancelar </button>
             </form>
         </div>
     </div>
