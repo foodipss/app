65e48c223f4a47dfc2b478c13b4fc770389376b1
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-27 18:40:39 +0100

    Update proc_registarV.php

--- proc_registarV.php
+++ proc_registarV.php
@@ -7,17 +7,14 @@
 session_start();
 include_once("conexaoPesquisa.php");
 date_default_timezone_set('Europe/London');
-
 $idtupperware = filter_input(INPUT_POST, 'idtupperware', FILTER_SANITIZE_NUMBER_INT);
 $idBeneficiario = filter_input(INPUT_POST, 'idBeneficiario', FILTER_SANITIZE_NUMBER_INT);
 $quantidadeLevou = filter_input(INPUT_POST, 'quantidadeLevou', FILTER_SANITIZE_NUMBER_INT);
 $quantidadeEntregue = filter_input(INPUT_POST, 'quantidadeEntregue', FILTER_SANITIZE_NUMBER_INT);
 $data = date("y/m/d H:i:s");
 $isRecolha=1;
-
 $result_tupperware = "INSERT INTO tupperware (idtupperware, idBeneficiario, quantidadeLevou, isRecolha, quantidadeEntregue, data) VALUES ('$idtupperware', '$idBeneficiario', '$quantidadeLevou', $isRecolha, '$quantidadeEntregue', '$data')";
 $resultado_tupperware = mysqli_query($conn, $result_tupperware) or die(mysqli_error($conn));
-
 if(mysqli_insert_id($conn)){
 	?>	
 <script> alert("Registado com sucessso");
@@ -30,4 +27,3 @@ top.location.href='pesquisarV.php';
 	top.location.href='pesquisarV.php';
 	</script>	
 <?php } ?>
-	
