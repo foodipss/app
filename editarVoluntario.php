d62357999e548db470a919519bfca3bf224cf26e
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-27 18:08:43 +0100

    Update editarVoluntario.php

--- editarVoluntario.php
+++ editarVoluntario.php
@@ -11,15 +11,60 @@ session_start();
   <?php require_once "index.php"; ?>
 </body>
 
-<!-- Bootstrap formulário -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
   <!-- Header -->
   <header class="w3-container" style="padding-top:22px">
-    <center><h5><b><i class="fa fa-dashboard"></i> Editar Informações </b></h5></center>
+    
+       <style>
+h5 {
+  color: black;
+  text-align: center;
+  font-family: ariana;
+  font-size: 30px;
+}
+input[type=text], select {
+	
+  width: 40%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=submit] {
+  width: 20%;
+  background-color: #FFBF00;
+  color: white;
+  padding: 14px 20px;
+  
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+input[type=submit]:hover {
+  background-color: #FF8000;
+}
+	      
+button[type=button] {
+  width: 20%;
+  background-color: #FFBF00;
+  color: white;
+  padding: 14px 20px;
+  
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+button[type=button]:hover {
+  background-color: #FF8000;
+}
+	      
+</style>
+   <center> <h5><b>Editar Voluntário</b></h5></center>  
+    
+    
+    
   </header>
 
   
@@ -39,43 +84,30 @@ session_start();
 
 
 <form action="editarVoluntario2.php" method="post" id="editarVoluntario2">
-<table id="tabelaEditarVoluntario">
-                  
-<table border="0" width="100%">
-  <tr>
+
     <input type="hidden" name="idVoluntario" value="<?php echo $idVoluntario; ?>">
     
-  </tr>
+  Nome:
+  <br>
+    <input type="text" name="nome"  value="<?php echo $row['nome']; ?>"/>
+<br/>
+  Telefone:
+  <br>
+    <input type="text" name="telefone" maxlength="9"  value="<?php echo $row['telefone']; ?>"/>
+   <br/>
+  Email:
+  <br>
+    <input type="text" name="email" value="<?php echo $row['email']; ?>"/>
+ <br/>
+  Morada:
+  <br>
+    <input type="text" name="morada" value="<?php echo $row['morada']; ?>"/>
+  <br/>
   
-  <tr>
-    <td width="20"><b>Nome:</b></td>
-    <td width="30"><input type="text" name="nome" size="25" value="<?php echo $row['nome']; ?>"/>
-    </td>
-  </tr>
-
-  <tr>
-    <td width="20"><b>Telefone:</b></td>
-    <td width="30"><input type="text" name="telefone" maxlength="9" size="25" value="<?php echo $row['telefone']; ?>"/>
-    </td>
-  </tr>
-  <tr>
-    <td width="20"><b>Email:</b></td>
-    <td width="30"><input type="text" name="email" value="<?php echo $row['email']; ?>"/>
-    </td>
-  </tr>
-  <tr>
-    <td width="20"><b>Morada:</b></td>
-    <td width="30"><input type="text" name="morada" value="<?php echo $row['morada']; ?>"/>
-    </td>
-  </tr>
-
-                </table>
-                <p></p>
                 <input type='hidden' name='query' value="NovoVoluntario">
                 <input type="submit" value="Concluir alterações" id="validar" class="btn btn-primary">
                 <button type="button" value="Voltar" onClick="history.go(-1)" class="btn btn-primary"> Cancelar </button>
             </form>
-        </div>
-    </div>
+   
 </body>
 </html>
