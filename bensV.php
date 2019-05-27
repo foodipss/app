c0cbe4068dbd6e83645c1a8a6abf39c9bbebcb29
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-27 18:26:26 +0100

    Add files via upload

---
+++ bensV.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<!DOCTYPE html>
+<html>
+<title>REFOOD</title>
+
+<?php require_once "index.php"; ?>
+
+<header class="w3-container" style="padding-top:40px">
+   
+     <style>
+h5 {
+  color: black;
+  text-align: center;
+  font-family: ariana;
+  font-size: 30px;
+}
+button {
+    width: 20%;
+    background-color: #FFBF00;
+    color: white;
+    padding: 14px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+   </style>
+   
+   <center> <h5><b> Lista de Bens </b></h5></center>
+    <p>
+  </header>
+
+<br>
+<?php
+// pegar a pagina atual
+$pagina = (isset($_GET['pagina'])) ? $_GET['pagina'] : 1;
+$itens_por_pagina = 7;
+$inicio = ($itens_por_pagina*$pagina)-$itens_por_pagina;
+$conn = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");;
+  
+  if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+  } 
+$sql = "SELECT * FROM bem WHERE visivel='1' LIMIT $inicio, $itens_por_pagina";
+$sql2 = "SELECT * from tipo";
+$result = $conn->query($sql) or die($conn->error);;
+date_default_timezone_set('Europe/London');
+$num_total = mysqli_num_rows($result);
+//defifnir o numero de paginas
+$num_paginas = ceil($num_total/$itens_por_pagina);
+//echo "The time is " . date("h:i:sa");
+  ?>
+
+
+<body>
+  <div class="colBens">
+  
+  <p></p>
+  <table class="table">
+
+    <thead>
+      <tr>
+  <th>Nome</th> 
+  <th>Tipo</th> 
+  <th>Fonte</th>
+  <th>Data</th>
+  <th>Número de porções</th>
+ </tr>
+    </thead>
+<tbody>
+    <?php while($row=$result->fetch_assoc()){?>
+      <tr>
+      <td><?php echo $row['nomeBem'];?>
+      </td>
+      <td>
+      <?php echo $row['tipoBem'];?>
+      </td>
+      <td>
+      <?php echo $row['idFonte'];?>
+      </td>
+      <td>
+      <?php echo $row['data'];?>
+      </td>
+  <td>
+      <?php echo $row['porcao'];?>
+      </td>
+  <td>
+  <a href="apagarBem.php?idBem=<?php echo $row['idBem'];?>" class="fa fa-trash-o" style="font-size:24px;color:red"></a>
+</td>
+</tr>
+<?php } ?>
+
+</tbody>
+
+
+
+</table>
+
+<?php
+$pagina_anterior = $pagina - 1;
+$pagina_seguinte = $pagina + 1;
+?>
+
+<nav class="text-center">
+  <ul class="pager">
+    <li class="previous">  
+
+    <?php 
+      if ($pagina_anterior != 0){ ?>
+
+      <a href="bens.php?pagina=<?php echo $pagina_anterior; ?>" >
+       <span class="glyphicon glyphicon-chevron-left" style="color:#000000"></span>
+      </a>
+
+    <?php }else { ?>
+      <span class="glyphicon glyphicon-chevron-left" style="color:#000000"></span>
+
+    <?php } ?>
+
+    </li>
+    <?php 
+      //Apresentar a paginação
+      for($i = 1; $i < $num_paginas; $i++) { ?>
+      <li><a href="bens.php?pagina=<?php echo $i; ?>"><?php echo $i ; ?></a></li>
+      <?php } ?>
+
+      <li class="next">  
+
+    <?php 
+      if ($pagina_seguinte <= $num_total){ ?>
+
+      <a href="bens.php?pagina=<?php echo $pagina_seguinte ; ?>">
+        <span class="glyphicon glyphicon-chevron-right" style="color:#000000"></span>
+      </a>
+
+    <?php }else { ?>
+      <span class="glyphicon glyphicon-chevron-right" style="color:#000000"></span>
+
+    <?php } ?>
+
+    </li>
+
+
+  </ul>
+</nav>
+
+
+     
+</div>
+<br>
+
+<form method="get" action="registarBens.php" >
+    <button type="submit">Registar Entrega de Bens</button>
+</form>
+
+ 
+
+</body>
+</html>
---
+++ registarBensV.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<title>REFOOD</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php
+require_once "index.php";
+$conn = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");;
+  
+  if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+  } 
+$sql = "SELECT idBem, nomeBem, data, idFonte, idTipo FROM bem WHERE visivel='1'";
+$result = $conn->query($sql);
+$sql2 = "SELECT idTipo, tipo FROM tipo";
+$result2= $conn->query($sql2);
+$sql3 = "SELECT * FROM fonte WHERE visivel='1'";
+$result3= $conn->query($sql3);
+  ?>
+
+<!-- Header -->
+  <header class="w3-container" style="padding-top:22px">
+    
+      <style>
+h5 {
+  color: black;
+  text-align: center;
+  font-family: ariana;
+  font-size: 30px;
+}
+input[type=text], select {
+	
+  width: 40%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+input[type=submit] {
+  width: 20%;
+  background-color: #FFBF00;
+  color: white;
+  padding: 14px 20px;
+  
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+input[type=submit]:hover {
+  background-color: #FF8000;
+}
+	      
+button[type=button] {
+  width: 20%;
+  background-color: #FFBF00;
+  color: white;
+  padding: 14px 20px;
+  
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+button[type=button]:hover {
+  background-color: #FF8000;
+}
+	      
+</style>
+   <center> <h5><b>Registar Entrega de Bens</b></h5></center>
+    <p>
+  </header>
+
+  <body>
+
+<form action="queryRegistarBem.php" method="post">
+  Nome do bem:
+	<br>
+	<input type="text" name="nomeBem" required>
+                  <br/>
+	<br>
+  Número de porções:
+	<br>
+	<input type="text" name="porcao" required>
+                  <br/>
+	<br>
+  Tipo do bem: 
+	<br>
+	<select name="tipoBem">
+    <?php while ($row=$result2->fetch_assoc()){  
+    echo "<option value='" . $row['tipo'] ."'>" . $row['tipo'] ."</option>";
+  } ?>
+  </select>
+  <br/>
+	<br>
+  Fonte:
+	<br>
+	<select name="idfonte">
+    <?php while ($row=$result3->fetch_assoc()){  
+    echo "<option value='" . $row['codigo_fonte'] ."'>" . $row['codigo_fonte'] ."</option>";
+  } ?>
+  </select>
+  <br/>
+	<br>
+
+  <input type="hidden" name="data" value="<?php echo date('d-m-Y H:i:s'); ?>" readonly="readonly">
+	<input type="submit" value="Registar Entrega de Bens">
+  <button type="button" value="Cancelar" onClick="history.go(-1)" class="btn btn-primary"> Cancelar </button>
+  
+
+            
+
+</form>       
+
+
+
+
+
+</body>
+</html>
