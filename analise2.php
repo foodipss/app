aa3e4eb47b509569ae8f2dc0151540fd81f85d64
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-27 18:16:23 +0100

    Update analise2.php

--- analise2.php
+++ analise2.php
@@ -13,7 +13,7 @@ include_once("conexaoPesquisa.php");
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <body>
-    <?php require_once "index.php"; ?>
+    <?php require_once "indexVoluntario.php"; ?>
     <br>
 
     <?php 
