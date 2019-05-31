63ad73c5c1628494f74d14da73ec539900056ebe
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-31 02:36:43 +0100

    Update ajuda.php

--- ajuda.php
+++ ajuda.php
@@ -15,6 +15,16 @@
  </head>
 
 <body> 
+  <?php
+    if (!isset($_SESSION['use'])) { // If session is not set then redirect to Login Page
+        ?>
+        <script>
+            window.location.href = "login.php";
+        </script>
+    <?php
+}
+
+?>
 
 <?php require_once "index.php"; ?>
 
