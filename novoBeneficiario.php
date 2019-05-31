342b07b4ba864313ec4b9b29e275e0b31dd5c1b9
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-31 02:50:07 +0100

    Update novoBeneficiario.php

--- novoBeneficiario.php
+++ novoBeneficiario.php
@@ -72,16 +72,7 @@ button[type=button]:hover {
   </header>
 
 	<body>
-		<?php
-    if (!isset($_SESSION['use'])) { // If session is not set then redirect to Login Page
-        ?>
-        <script>
-            window.location.href = "login.php";
-        </script>
-    <?php
-}
 
-?>
   
             <form action="QueryBeneficiario.php" method="post" id="beneficiario">
               
