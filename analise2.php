73f15b73a037b4784124c0b5a1b43e30ad97cc03
ritaabsilva <49485885+ritaabsilva@users.noreply.github.com>
2019-05-31 14:51:07 +0100

    Add files via upload

--- analise2.php
+++ analise2.php
@@ -1,22 +1,54 @@
 <?php
 session_start();
 include_once("conexaoPesquisa.php");
-?>
-
+ $connect2 = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "b74a37105022bd", "c0139137", "heroku_af588fa1a66d1f3");  
+ $query2 = "SELECT idFonte, count(*) as number FROM bem GROUP BY idFonte";  
+ $result2 = mysqli_query($connect2, $query2); 
+ ?>
 
 <html>
 <head>
+  <title>REFOOD</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/estilo.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <body>
-    <?php require_once "indexVoluntario.php"; ?>
-    <br>
 
-    <?php 
+    <?php require_once "indexVoluntario.php"; ?>
+
+  <h1 style="text-align: center;">Dados Estatisticos</h1>
+
+      <div style="width: 100%;">
+        <div style="width: 50%;">   
+           <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
+           <script type="text/javascript">  
+           google.charts.load('current', {'packages':['corechart']});  
+           google.charts.setOnLoadCallback(drawChart);  
+           function drawChart()  
+           {  
+                var data = google.visualization.arrayToDataTable([  
+                          ['idFonte', 'number'],  
+                          <?php  
+                          while($row = mysqli_fetch_array($result2))  
+                          {  
+                               echo "['".$row["idFonte"]."', ".$row["number"]."],";  
+                          }  
+                          ?>  
+                     ]);  
+                var options = {  
+                      title: 'Fontes que doaram o maior numero de bens:',  
+                      is3D:true,  
+                      pieHole: 0.4  
+                     };  
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));  
+                chart.draw(data, options);  
+           }  
+           </script> 
+          </div>
+      <div style="width: 50%;">
+              <?php 
      
       $lista = array();
       $devia = array();
@@ -31,7 +63,6 @@ include_once("conexaoPesquisa.php");
       $sql = "SELECT codigo_beneficiario, deve from beneficiario order by deve desc";
       $resultado = $conn->query($sql);
       while ($row = mysqli_fetch_object($resultado)) {
-
         $codigo_beneficiario = $row->codigo_beneficiario;
         $deve = $row->deve;
         $lista[$i] = $codigo_beneficiario;
@@ -51,9 +82,7 @@ include_once("conexaoPesquisa.php");
         $k = 5;
         for ($i= 0; $i < $k; $i++) {
         ?>
-
             ['<?php echo $lista[$i] ?>', <?php arsort($devia); echo $devia[$i]; ?>, '<?php echo $cor[$i] ?>'],        
-
         <?php } ?>
       ]);
       var view = new google.visualization.DataView(data);
@@ -63,7 +92,6 @@ include_once("conexaoPesquisa.php");
                          type: "string",
                          role: "annotation" },
                        2]);
-
       var options = {
         title: "Beneficiários com mais tupperware em falta",
         width: 600,
@@ -76,38 +104,13 @@ include_once("conexaoPesquisa.php");
   }
   </script>
 
-    
+    </div>
 
-  <div id="columnchart_values" style="width: 50%; float: right; padding: 5px;"></div>
-
-     <script type="text/javascript">
-      google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['F3',     50],
-          ['F2',      20],
-          ['F8',  20],
-          ['F4', 10],
-          ['F1',    7]
-          //['<?php $codigo_fonte ?>', <?php $contador?>],        
-
-      ]);
-        var options = {
-          title: 'Fontes que doaram o maior número de bens',
-           width: 600,
-           height: 400,
-          pieHole: 0.4,
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options);
-      }
-    </script>
-</head>
-  <body>
-    <?php
+  <div id="columnchart_values" style="width: 50%; height: 50%; float: right; padding: 5px;"></div> 
+       
+ </head> 
+ <body> 
+  <?php
     if (!isset($_SESSION['use'])) { // If session is not set then redirect to Login Page
         ?>
         <script>
@@ -115,8 +118,15 @@ include_once("conexaoPesquisa.php");
         </script>
     <?php
 }
-
 ?>
-  <div id="donutchart" style="width: 50%; height: 50%; padding: 5px; "></div>
-  </body>
-</html>
+           <div style="width:100%;">  
+              
+                
+                <div id="piechart" style="width: 50%; height: 50%; float: left; padding: 5px;"></div>  
+           </div>  
+
+           <br/>
+
+      </body> 
+      </div> 
+ </html>  
