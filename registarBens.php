447124483d63d944df177dee29fa9b03c3263a98
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-31 02:37:42 +0100

    Update registarBens.php

--- registarBens.php
+++ registarBens.php
@@ -80,7 +80,16 @@ button[type=button]:hover {
   </header>
 
   <body>
+<?php
+    if (!isset($_SESSION['use'])) { // If session is not set then redirect to Login Page
+        ?>
+        <script>
+            window.location.href = "login.php";
+        </script>
+    <?php
+}
 
+?>
 <form action="queryRegistarBem.php" method="post">
   Nome do bem:
 	<br>
