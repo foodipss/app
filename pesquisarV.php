4d457e909823f5b952a4e060f188b60985686bfd
uptech1 <49435894+uptech1@users.noreply.github.com>
2019-05-31 02:51:13 +0100

    Update pesquisarV.php

--- pesquisarV.php
+++ pesquisarV.php
@@ -62,16 +62,6 @@ input[type=submit]:hover {
 </script>
 
 	<body>
-		<?php
-    if (!isset($_SESSION['use'])) { // If session is not set then redirect to Login Page
-        ?>
-        <script>
-            window.location.href = "login.php";
-        </script>
-    <?php
-}
-
-?>
 		<?php require_once "indexVoluntario.php"; ?>
 	
 		
